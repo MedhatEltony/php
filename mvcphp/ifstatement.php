879a65bcec6af71117e0864bf3836740476f67f3
unknown <me.muslim.coder@gmail.com>
2021-02-27 15:08:28 +0300

    add array file

--- mvcphp/ifstatement.php
+++ mvcphp/ifstatement.php
@@ -50,10 +50,59 @@ switch ($carSpeed) {
         echo '';
 }
 
-//loop 
-    //while 
-    //do-while
-    //for 
-    //foreach - to be discussed after studying arrays 
+//loop
+// initializer , condition , increment/decrement
+
+// EX: The player case above 
+
+
+
+//while 
+$playerAgeloob = 16;
+
+while ($playerAgeloob <= 32) {
+    echo '<p> The player is ' . $playerAgeloob . ' years old can stull player';
+    $playerAgeloob++;
+}
+
+$temp = 15;
+while ($temp >= 5) {
+    echo 'I can still survive the  tempreature is';
+    $temp--;
+}
+//do-while
+$student = 10;
+
+do {
+    echo '<p> I will give present</p>';
+    $student--;
+} while ($student >= 10);
+
+//for 
+
+for ($age = 16; $age <= 35; $age++) {
+    if ($age == 20) {
+        continue;
+    }
+
+    if ($age == 30) {
+        goto warning;
+        //go to line 106
+    }
+
+    if ($age == 33) {
+        break;
+    }
+    echo '<p> The player is ' . $age . ' years old and still play';
+}
+
+
+
+//foreach - to be discussed after studying arrays 
+// after to learn arrays 
+
 //break and goto 
-//countu
+
+warning:
+echo '<p>Take Care</p>';
+//continue
